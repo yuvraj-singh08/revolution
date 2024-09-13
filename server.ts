89cb3eed61d33109caf47b705545aa08cb27b6c2
Yuvraj Singh <yuvrajsinghwahla08cs@gmail.com>
2024-09-13 14:40:08 +0530

    Added ping api

--- server.ts
+++ server.ts
@@ -1,13 +1,13 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
 import { sequelize } from './models';
 require('dotenv').config(); // For environment variables
 
 //Routers
 import driverRouter from './routes/driver.routes';
-import rolesRouter  from './routes/role.routes';
-import adminRouter  from './routes/admin.routes';
-import stopRouter  from './routes/stop.routes';
+import rolesRouter from './routes/role.routes';
+import adminRouter from './routes/admin.routes';
+import stopRouter from './routes/stop.routes';
 
 
 const app = express();
@@ -19,10 +19,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+app.get('/', (req: Request, res: Response) => {
+    try {
+        res.status(200).send("Server is up and running");
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Server error");
+    }
+})
+
 //Routes
 app.use('/api/driver', driverRouter);
 app.use('/api/role', rolesRouter);
-app.use('/api/admin',adminRouter);
+app.use('/api/admin', adminRouter);
 app.use('/api/stop', stopRouter);
 
 import './models/associations/index';
