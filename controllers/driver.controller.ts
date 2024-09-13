604dfec3f2a5eb890ec0de64c388ca32a9a77179
vai3hav.sh <vai3hav.sh@gmail.com>
2024-09-13 22:59:24 +0530

    mailer turned on

--- controllers/driver.controller.ts
+++ controllers/driver.controller.ts
@@ -46,23 +46,23 @@ export const createDriver = async (req: AuthenticatedRequest, res: Response, nex
 //     }
 // });
 
-// const mailOptions = {
-//     from: process.env.MAILER_ID,
-//     to: req.body.email,
-//     subject: 'Registration Successful',
-//     text: `Hello ${ req.body.name }, \n\nYour registration was successful!\n\nHere are your credentials: \n\nEmail: ${ req.body.email }\nPassword: ${ req.body.password }\n\nPlease change your password after logging in.\n\nBest regards, \nYour Company`
-// };
+const mailOptions = {
+    from: process.env.MAILER_ID,
+    to: req.body.email,
+    subject: 'Registration Successful',
+    text: `Hello ${ req.body.name }, \n\nYour registration was successful!\n\nHere are your credentials: \n\nEmail: ${ req.body.email }\nPassword: ${ req.body.password }\n\nPlease change your password after logging in.\n\nBest regards, \nYour Company`
+};
 
-// // Send the email and commit only if the email is sent successfully
-// transporter.sendMail(mailOptions, async (error: Error | null, info: { response: string }) => {
-//     if (error) {
-//         console.log('Error sending email:', error);
-//         return res.send({ message: 'Registration successful, but failed to send email', error: true });
-//     } else {
-//         console.log('Email sent:', info.response);
-//         return res.send({ message: 'Registration successful and email sent', error: false });
-//     }
-// });
+// Send the email and commit only if the email is sent successfully
+transporter.sendMail(mailOptions, async (error: Error | null, info: { response: string }) => {
+    if (error) {
+        console.log('Error sending email:', error);
+        return res.send({ message: 'Registration successful, but failed to send email', error: true });
+    } else {
+        console.log('Email sent:', info.response);
+        return res.send({ message: 'Registration successful and email sent', error: false });
+    }
+});
 
 
         return res.status(201).json({ success: true, message: "Created New Driver", data: createdDriver });
