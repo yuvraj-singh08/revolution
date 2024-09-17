969a3f2a071c69a8bc8b7b8de61420c8c3a52920
Yuvraj Singh <yuvrajsinghwahla08cs@gmail.com>
2024-09-17 17:00:06 +0530

    Fix

--- controllers/route.controller.ts
+++ controllers/route.controller.ts
@@ -92,7 +92,7 @@ export const getAssignedRoutes = async (req: AuthenticatedRequest, res: Response
             return;
         }
         const assignedRoutes = await getAssignedRoutesService(driverId, date);
-        res.status(200).json({ success: true, data: assignedRoutes });
+        res.status(200).json({ success: true, ...assignedRoutes });
     } catch (error: any) {
         res.status(500).json({
             success: false,
