9787724abed07e529affcef4340e0c0b998d8538
Yuvraj Singh <yuvrajsinghwahla08cs@gmail.com>
2024-11-07 17:32:03 +0530

    Fix

--- models/index.ts
+++ models/index.ts
@@ -10,13 +10,16 @@ export const sequelize = new Sequelize(db.database as string, db.username as str
     dialect: 'mysql',
     port: db.port ?? 3306,
     pool: {
-        max: 10, // Maximum number of connections in the pool
-        min: 0, // Minimum number of connections in the pool
-        acquire: 50000, // Maximum time (ms) Sequelize will try to get a connection before throwing an error
-        idle: 10000, // Maximum time (ms) a connection can be idle before being released
+        max: 10,
+        min: 0,
+        acquire: 50000,
+        idle: 10000,
     },
-    logging: false, // Disable logging in production
+    logging: false,
     retry: {
-        max: 3, // Retry a failed query up to 3 times
+        max: 3,
+    },
+    dialectOptions: {
+        connectTimeout: 60000, // 60 seconds
     },
 });
